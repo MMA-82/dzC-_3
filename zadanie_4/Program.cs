faaf0e2c291989c79f3db1230be944e7c634f96f
Marat <brutto061@gmail.com>
2022-10-10 23:51:37 +0700

    Выполнили четвертое задание

---
+++ zadanie_4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+if(N%10%2 == 0)
+{
+    Console.WriteLine("Куб введенного числа равен: " + Math.Pow(N, 3));
+}
+else Console.WriteLine("У введенного числа последняя цифра нечетная!");
