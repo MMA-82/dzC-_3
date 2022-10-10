0d912794eb7b33392847d081147f085ae0d1cd1a
Marat <brutto061@gmail.com>
2022-10-10 23:33:08 +0700

    Выполнили третье задание

---
+++ zadanie_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+int result = 1;
+if(N>0)
+{
+while(i <= N)
+{
+    result = result * i;
+    i++;        
+}
+Console.WriteLine("Произведение всех чисел равно: " + result);
+}
+else Console.WriteLine("Вы ввели отрицательное число!!!"); 
