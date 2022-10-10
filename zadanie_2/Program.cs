d95fd17e664e0e37ad710aff83be48aa7a8aca91
Marat <brutto061@gmail.com>
2022-10-10 23:09:10 +0700

    Выполнили второе задание

---
+++ zadanie_2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+if(x>9)
+{
+    while(x > 0)
+    {
+        sum = sum + x%10;
+        x = x/10;
+    }
+    Console.WriteLine("Сумма цифр числа равна: " + sum);
+}
+else 
+{
+    if(x>=0) 
+    {
+    Console.WriteLine("Сумма цифр числа равна: " + x);
+    }
+    else Console.WriteLine("Введено отрицательное число!!!");
+}
+    
